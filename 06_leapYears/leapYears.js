04bb6561b80175bb55af01da59c5ad638ae9d849
Devin Raleigh <git@dkr.im>
2022-02-04 15:52:12 -0500

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// Year is a leap year if it is divisible by 4 but not divisible by 100 except when divisible by 400.
+//    if ((year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0))) {
+//        return true;
+//    }
+//    else {
+//        return false;
+//    }
+    // Rewrite above as ternary.
+    return ((year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0))) ? true : false;
 };
 
 // Do not edit below this line
