8d82e88347c6799566d502f0accdc90113df4e2e
Devin Raleigh <git@dkr.im>
2022-02-04 15:51:55 -0500

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return "ERROR";
+    }
+    else if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (a <= b) {
+        while (a <= b) {
+            sum += a;
+            a++;
+        }
+        return sum;
+    }
+    else if (a > b) {
+        while (a >= b) {
+            sum += a;
+            a--;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
